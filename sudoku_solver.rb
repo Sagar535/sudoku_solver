d0974079fb3075be4caea88e651f570e2c1ac8c5
sagar535 <dsl.sagar535@gmail.com>
2024-05-11 18:37:10 +0545

    WIP

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -7,7 +7,6 @@ def sudoku_solver(board)
   loops = (0..8).to_a
 
   # assign possible values 1 to 9 to each cell that doesn't hold any value
-
   loops.each do |first_loop|
     loops.each do |second_loop|
       board[first_loop][second_loop] ||= (1..9).to_a
@@ -20,36 +19,82 @@ def sudoku_solver(board)
 
   until solved
 
+    value_changed = false
+
     p "solved: #{solved}"
     loops.each do |first_loop|
+      rows = get_rows(first_loop)
       loops.each do |second_loop|
-        cell = board[first_loop][second_loop]
-        # eliminate from the related rows
-        loops.each do |row_loop|
-          next if row_loop == second_loop # skip if we are in same cell
+        columns = get_columns(second_loop)
 
-          row_cell = board[first_loop][row_loop]
+        #  cell that is currently being checked for selectively trimming the values
+        cell = board[first_loop][second_loop]
+
+        rows.each do |row|
+          rx, ry = row
+          next if ry == second_loop # skip if we are in same cell
+
+          row_cell = board[rx][ry]
+
           next if row_cell.integer?
 
-          board[first_loop][row_loop] -= [cell] if row_cell.include?(cell)
-
-          board[first_loop][row_loop] = board[first_loop][row_loop].first if board[first_loop][row_loop].count == 1
+          # include? will check for each cell contains the number
+          # FIXME: we can run the following only if cell is integer
+          if cell.integer? && row_cell.include?(cell)
+            board[rx][ry] -= [cell]
+            value_changed = true
+          end
         end
 
-        # eliminate from the related columns
-        loops.each do |column_loop|
-          next if column_loop == first_loop # skip if we are in same cell
+        columns.each do |column|
+          cx, cy = column
+          next if cy == first_loop # skip if we are in same cell
 
-          column_cell = board[column_loop][second_loop]
+          column_cell = board[cx][cy]
+
           next if column_cell.integer?
 
-          board[column_loop][second_loop] -= [cell] if column_cell.include?(cell)
-
-          board[column_loop][second_loop] = board[column_loop][second_loop].first if board[column_loop][second_loop].count == 1
+          # include? will check for each cell contains the number
+          # FIXME: we can run the following only if cell is integer
+          if cell.integer? && column_cell.include?(cell)
+            board[cx][cy] -= [cell]
+            value_changed = true
+          end
         end
 
+        # boxes = box_coordinates(first)
+        # eliminate from the related rows
+        # loops.each do |row_loop|
+        #   next if row_loop == second_loop # skip if we are in same cell
+
+        #   row_cell = board[first_loop][row_loop]
+        #   next if row_cell.integer?
+
+        #   board[first_loop][row_loop] -= [cell] if row_cell.include?(cell)
+
+        #   if board[first_loop][row_loop].count == 1
+        #     board[first_loop][row_loop] = board[first_loop][row_loop].first
+        #     value_changed = true
+        #   end
+        # end
+
+        # eliminate from the related columns
+        # loops.each do |column_loop|
+        #   next if column_loop == first_loop # skip if we are in same cell
+
+        #   column_cell = board[column_loop][second_loop]
+        #   next if column_cell.integer?
+
+        #   board[column_loop][second_loop] -= [cell] if column_cell.include?(cell)
+
+        #   if board[column_loop][second_loop].count == 1
+        #     board[column_loop][second_loop] = board[column_loop][second_loop].first
+        #     value_changed = true
+        #   end
+        # end
+
         # eliminate from the box
-        box = box_finder(first_loop, second_loop)
+        box = box_coordinates(first_loop, second_loop)
 
         box.each do |xy|
           next if xy == [first_loop, second_loop]
@@ -59,28 +104,133 @@ def sudoku_solver(board)
 
           board[x][y] -= [cell] if box_cell.include?(cell)
 
-          board[x][y] = board[x][y].first if board[x][y].count == 1
+          if board[x][y].count == 1
+            value_changed = true
+            board[x][y] = board[x][y].first
+          end
         end
       end
     end
 
+    # run these loops only if there is no change in cell values
+    unless value_changed
+      # set as that value
+      # if only repeated once in row
+      loops.each do |first_loop|
+        row = board[first_loop]
+        value_changed = false
+
+        row.each_with_index do |cell, index|
+          value = nil
+          next if cell.integer?
+          next if value_changed
+
+          row.each do |other_cell|
+            next if other_cell == cell
+            if other_cell.integer?
+              board[first_loop][index] -= [other_cell]
+            else
+              value = cell - other_cell if value.nil?
+
+              value = value - other_cell
+            end
+          end
+
+          if value&.size == 1
+            p "first_loop: #{first_loop}, index: #{index}"
+            value_changed = true
+            board[first_loop][index] = value.first
+          end
+        end
+      end
+
+      # # set as that value
+      # # if only repeated once in column
+
+      # loops.each do |first_loop|
+      #   column = []
+      #   loops.each do |second_loop|
+      #     column << board[second_loop][first_loop]
+      #   end
+
+      #   # p "unique in column #{first_loop}: "
+      #   column.each_with_index do |cell, index|
+      #     value = nil
+      #     next if cell.integer?
+
+      #     column.each do |other_cell|
+      #       next if other_cell == cell
+      #       next if other_cell.integer?
+
+      #       value = cell - other_cell if value.nil?
+
+      #       value = value - other_cell
+      #     end
+
+      #     if value&.size == 1
+      #       board[first_loop][index] = value.first
+      #     end
+      #   end
+      # end
+
+      # set as that value
+      # if only repeated once in box
+
+      # take each coordinate for each box
+      [[0,0], [0, 2], [0, 6], [3, 0], [3, 3], [3, 6], [6, 0], [6, 3], [6, 6]].each do |ij|
+        i, j = ij
+
+        # p "i: #{i}, j: #{j}"
+
+        box = box_coordinates(i, j)
+        value_changed = false
+        box.each do |xy|
+          value = nil
+          x,y = xy
+          # p "x: #{x}, y: #{y}"
+          next if value_changed
+          next if board[x][y].integer?
+          box.each do |ab|
+            a, b = ab
+            # p "a: #{a}, b: #{b}"
+            next if a == x and b == y
+            if board[a][b].integer?
+              board[x][y] -= [board[a][b]]
+            else
+              value = board[x][y] - board[a][b] if value.nil?
+
+              value = value - board[a][b]
+            end
+          end
+
+          if value&.size == 1
+            p "x: #{x}, y: #{y}"
+            board[x][y] = value.first
+            value_changed = true
+          end
+        end
+      end
+    end
+
+    p "Loop count: #{loop_count}"
+    p "THE BOARD:"
+    board_printer(board)
+
+
     loop_count += 1
     solved = loop_count == 100
   end
-
-  p board
 end
 
 # TODO: FIXME: Complete it so that we can print the board at last
 def board_printer(board)
-  loops = (0..8).to_a
-
-  loops.each do |first_loop|
+  board.each do |row|
+    p row
   end
 end
 
 # cell will be array of
-def box_finder(row, column)
+def box_coordinates(row, column)
   relative_position = find_relative_position(row, column)
 
   first_box = [
@@ -134,6 +284,26 @@ def find_box_number(row, column)
   box = box.intersection [3, 6, 9] if [6, 7, 8].include? column
 
   box.flatten.first
+end
+
+# row coordinates
+def get_rows(row)
+  rows = []
+  (0..8).to_a.each do |n|
+    rows << [row, n]
+  end
+
+  rows
+end
+
+# column coordinates
+def get_columns(column)
+  columns = []
+  (0..8).to_a.each do |n|
+    columns << [n, column]
+  end
+
+  columns
 end
 
 class Array
